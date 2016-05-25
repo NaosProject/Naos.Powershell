81687b62f0c9aebe56b9624d6887bc10fb3c716b
Lawson Caudill <lawson@caudillclan.us>
2016-05-25 13:49:45 -0400

    Added build repo example for appveyor build script.

--- Build/AppveyorBuildScript-Build.ps1
+++ Build/AppveyorBuildScript-Build.ps1
@@ -1,6 +1,6 @@
 # PASTE THIS INTO APPVEYOUR
 
 rm *.nupkg # make sure one doesnt get checked in by accident and used...
-nuget pack ./Build/Naos.Powershell.Build.nuspec -Version $env:APPVEYOR_BUILD_VERSION
+nuget pack ./Naos.Build.nuspec -Version $env:APPVEYOR_BUILD_VERSION
 $nupkgFile = (ls . -Filter *.nupkg).FullName
 nuget push $nupkgFile $env:nuget_api_key -Source $env:nuget_gallery_url
