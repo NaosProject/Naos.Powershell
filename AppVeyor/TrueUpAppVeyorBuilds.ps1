dfbc8ac2c7fb5104c1274e164652a34fdae236dc
Lawson Caudill <lawson@getthinktank.com>
2020-10-22 15:25:07 +0100

    Added backup logic to write a copy of existing yaml to disk and also a dryrun switch to make sure the projects being updated and the resultant yaml are what is expected before making changes.

--- AppVeyor/TrueUpAppVeyorBuilds.ps1
+++ AppVeyor/TrueUpAppVeyorBuilds.ps1
@@ -15,13 +15,19 @@ The name of a single project to true up to reference (optional, default is ALL p
 The AppVeyor provided API Token.
 
 .PARAMETER BlackList
-A collection of project names to exclude from.
+A collection of project names to exclude from; e.g. $(,'Naos.Build','Naos.Powershell','Naos.Recipes','Naos.Configuration').
+
+.PARAMETER BackupDirectory
+If supplied it will backup the existing build YAML as a file in the directory before updating.
 
 .PARAMETER RunForTarget
 Switch to initiate execution, will only work on a target project.
 
 .PARAMETER RunForAll
 Switch to initiate execution, will only work without a target project.
+
+.PARAMETER DryRun
+Switch to cause the script to run and print out what 'would' be sent to AppVeyor but not actually updating it.
 
 .EXAMPLE
 .\TrueUpAppVeyorBuilds.ps1 -ReferenceProjectName 'Naos.Utils.Database' -Run
@@ -31,23 +37,41 @@ param(
 	[string] $ReferenceProjectName = $(throw 'Must provide a ReferenceProjectName'),
 	[string] $TargetProjectName = $null,
 	[string] $ApiToken = $(throw 'Must provide an ApiToken'),
-	[Array]  $BlackList = @('asdfasdfasdf'),
+	[Array]  $BlackList = $null,
+	[string] $BackupDirectory = $null,
 	[switch] $RunForTarget,
-	[switch] $RunForAll
+	[switch] $RunForAll,
+	[switch] $DryRun
 )
 try
 {
-	$BlackList | %{
-		if ($ReferenceProjectName -eq $_)
-		{
-			throw "Can't use a blacklist project ($_) as a reference project..."
-		}
-		
-		if ($TargetProjectName -eq $_)
-		{
-			throw "Can't use a blacklist project ($_) as a target project..."
-		}
-	}
+    if (-not [string]::IsNullOrEmpty($BackupDirectory))
+    {
+        if (-not (Test-Path $BackupDirectory))
+        {
+            throw "Please ensure backup directory '$BackupDirectory' exists before running"
+        }
+    }
+
+    $internalBlackList = New-Object 'System.Collections.Generic.List[String]'
+    if ($BlackList -ne $null)
+    {
+        Write-Host '- Blacklist Entries'
+        $BlackList | %{
+            if ($ReferenceProjectName -eq $_)
+            {
+                throw "Can't use a blacklist project ($_) as a reference project..."
+            }
+            
+            if ($TargetProjectName -eq $_)
+            {
+                throw "Can't use a blacklist project ($_) as a target project..."
+            }
+            
+            $internalBlackList.Add($_)
+            Write-Host "-- $_"
+        }
+    }
 	
 	if ($RunForAll -and (-not [String]::IsNullOrEmpty($TargetProjectName)))
 	{
@@ -67,8 +91,10 @@ try
 		  "Accept" = "application/json"
 		}
 
+        $scriptBackupFileTimeStamp = [DateTime]::UtcNow.ToString('yyyyMMdd_HHmm')
+
 		$projects = Invoke-RestMethod -Uri 'https://ci.appveyor.com/api/projects' -Headers $headers -Method Get
-		$projects = $projects | ?{ -not $BlackList.Contains($_.name) }
+		$projects = $projects | ?{ -not $internalBlackList.Contains($_.name) }
 		
 		$referenceProject = $projects | ?{ $_.name -eq $ReferenceProjectName }
 		$referenceProjectSettingsUrl = "https://ci.appveyor.com/api/projects/$($referenceProject.accountName)/$($referenceProject.slug)/settings/yaml"
@@ -85,6 +111,14 @@ try
                 # Make sure that we do not overwrite unique settings (like the build version pattern which could have been manually manipulated)
                 $existingProjectSettingsUrl = "https://ci.appveyor.com/api/projects/$($_.accountName)/$($_.slug)/settings/yaml"
                 $existingProjectYaml = Invoke-RestMethod -Uri $existingProjectSettingsUrl -Headers $headers -Method Get -ContentType "plain/text"
+                if (-not [string]::IsNullOrEmpty($BackupDirectory))
+                {
+                    $backupFileName = "$($scriptBackupFileTimeStamp)___$($_.slug).yml"
+                    $backupFilePath = Join-Path $BackupDirectory $backupFileName
+                    $existingProjectYaml | Out-File -FilePath $backupFilePath -Encoding UTF8
+                    Write-Host "-- BackupDirectory set, $($_.name) backed up to '$backupFilePath'"
+                }
+                
                 $existingBuild = $existingProjectYaml.Split([Environment]::NewLine)[0]
 
                 $newYaml = $referenceProjectYaml.Replace($referenceBuild, $existingBuild)
@@ -97,15 +131,22 @@ try
                 Write-Output $newYaml
                 Write-Host -ForegroundColor Magenta "<-END updated YAML"
 
-				$putResponse = Invoke-WebRequest -Uri $settingsUrl -Headers $headers -Method Put -ContentType "plain/text" -Body $newYaml
-				$responseCode = $putResponse.StatusCode
+                if ($DryRun)
+                {
+                    Write-Host "-- Skipping actual update as DryRun switch was set."
+                }
+                else
+                {
+                    $putResponse = Invoke-WebRequest -Uri $settingsUrl -Headers $headers -Method Put -ContentType "plain/text" -Body $newYaml
+                    $responseCode = $putResponse.StatusCode
 
-				Write-Host "<Response was '$responseCode' (204 is expected if successful)"
+                    Write-Host "-- Response was '$responseCode' (204 is expected if successful)"
 
-				if ($responseCode -ne '204')
-				{
-					throw "Expected a response code of 204 and instead got $responseCode, please investigate..."
-				}
+                    if ($responseCode -ne '204')
+                    {
+                        throw "Expected a response code of 204 and instead got $responseCode, please investigate..."
+                    }
+                }
 				
                 Write-Host -ForegroundColor Magenta "<END Updating $($_.name) with new YAML; updated from ($referenceProjectName)"
 				Write-Host ''
