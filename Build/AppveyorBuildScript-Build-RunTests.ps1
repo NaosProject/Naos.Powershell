91ecc4fbbec19cc905d5e806ac2d92d5c974bf4c
Lawson Caudill <lawson@getthinktank.com>
2022-07-14 12:18:50 -0400

    Fix weird unicode characters that were failing in powershell and updated empty strings from "" to ''.

--- Build/AppveyorBuildScript-Build-RunTests.ps1
+++ Build/AppveyorBuildScript-Build-RunTests.ps1
@@ -3,23 +3,23 @@ $path = Resolve-Path .
  $hasTestFailure = $false
  $csProjFilePaths = ls $path -filter "*.$projectFileExtension" -recurse | %{$_.FullName} | ?{$_.ToLower().EndsWith(".test.$projectFileExtension") -or $_.ToLower().EndsWith(".tests.$projectFileExtension")}
  
- Write-Output ''
+ Write-Output ""
  Write-Output "================ STARTING TESTING ================"
  $csProjFilePaths | %{
      Write-Output "Discovered test project: $_"
  }
-​
+
  $csProjFilePaths | %{
     $csProjFilePath = $_
     $csProjFileName = Split-Path $csProjFilePath -Leaf
     $csProjDirectoryPath = Split-Path $csProjFilePath
-    $csProjAssemblyPathWithoutExtension = Join-Path (Join-Path $csProjDirectoryPath 'bin\Debug') $csProjFileName.Replace(".$projectFileExtension", '')
+    $csProjAssemblyPathWithoutExtension = Join-Path (Join-Path $csProjDirectoryPath 'bin\Debug') $csProjFileName.Replace(".$projectFileExtension", "")
     $exePath = "$csProjAssemblyPathWithoutExtension.exe"
     $DllPath = "$csProjAssemblyPathWithoutExtension.dll"
-​
-    Write-Output ''
+
+    Write-Output ""
     Write-Output "================ START TEST $csProjFileName ================"
-​
+
     if (Test-Path $exePath)
     {
         &(Join-Path $env:xunit20 'xunit.console.x86') $exePath -appveyor
@@ -40,17 +40,17 @@ $path = Resolve-Path .
     {
         throw "Expected to either find assembly: $exePath or $dllPath.  Neither were found, please verify that the output assembly is named the same as the project."
     }
-​
+
     Write-Output "================ END TEST $csProjFileName ================"
-    Write-Output ''
+    Write-Output ""
  }
-​
+
  if ($hasTestFailure)
  {
      throw "A unit test has failed.  Test runner exited with non-0 exit code.  See output above for details."
  }
  else
  {
-     Write-Output ''
+     Write-Output ""
      Write-Output "================ COMPLETED TESTING ================"
  }
